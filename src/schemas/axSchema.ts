d5ec7f835ef25fc648a9a07a155cac91a0a26923
Asadbek <asadbek010azamjonov@gmail.com>
2024-11-21 21:25:19 +0100

    updated axSchema

--- src/schemas/axSchema.ts
+++ src/schemas/axSchema.ts
@@ -3,6 +3,7 @@ import { IAxInterface } from '../types/IaxInterface'
 
 const IAxSchema = new Schema<IAxInterface>(
 	{
+		number: { type: Number, required: true },
 		arabicName: { type: String, required: true },
 		translation: { type: String, required: true },
 		transliteration: { type: String, required: true, unique: true },
