7ad546982ad23c09b8fff1e58588897cece8b03f
Asadbek <asadbek010azamjonov@gmail.com>
2024-12-06 21:14:56 +0100

    #11 email sending

--- src/app.ts
+++ src/app.ts
@@ -5,24 +5,32 @@ import userRouter from './routes/userRoute'
 import axRouter from './routes/axRoute'
 import axGameRoute from './routes/axGameRoute'
 import blogRouter from './routes/blogRoute'
+
 const server = express()
 
-const whitelist = ['http://localhost:3000']
+// Define the allowed origins
+const whitelist = [
+	'http://localhost:3000',
+	'https://azamjonov.com',
+	'https://www.azamjonov.com',
+]
 
-// const corsOptions = {
-//   origin: function (origin: any, callback: any) {
-//     if (whitelist.indexOf(origin) !== -1) {
-//       callback(null, true)
-//     } else {
-//       callback(new Error('Not allowed by CORS'))
-//     }},
-//     credentials: true
-// }
+// CORS options
+const corsOptions = {
+	origin: function (origin: any, callback: any) {
+		if (whitelist.indexOf(origin) !== -1 || !origin) {
+			// Check if the origin is in the whitelist
+			callback(null, true)
+		} else {
+			callback(new Error('Not allowed by CORS'))
+		}
+	},
+	credentials: true,
+}
 
 // Middlewares
 server.use(express.json())
-server.use(cors({ origin: 'https://azamjonov.com', credentials: true }))
-// server.use(cors())
+server.use(cors(corsOptions))
 server.use(cookieParser())
 
 // Routes
